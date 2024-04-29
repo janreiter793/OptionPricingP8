f73ea553c9729f3d2f149ba853089827a2dd9e08
janreiter793 <70901834+janreiter793@users.noreply.github.com>
2024-04-29 13:27:09 +0200

    Update squaredErrorsBS.R

--- squaredErrorsBS.R
+++ squaredErrorsBS.R
@@ -3,11 +3,11 @@ library(tidyquant)
 library(magrittr)
 
 # Parameters
-files <- c("C:/Users/janre/Documents/uni/8. Semester/Projekt/data/AMZN_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/BRK-B_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/GOOG_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/GOOGL_options.csv")
-r    <- 0.00 # US treasury bond yield length one year minus inflation rate as
+files <- c("~/data/AMZN_options.csv",
+           "~/data/BRK-B_options.csv",
+           "~/data/GOOG_options.csv",
+           "~/data/GOOGL_options.csv")
+r    <- 0.01711 # US treasury bond yield length one year minus inflation rate as
               # of 29-04-2024.
 
 # Takes an option symbol and returns the associated underlying asset symbol
