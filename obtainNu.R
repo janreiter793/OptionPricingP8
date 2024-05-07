e3b1bf203dac373630c40f45a2a546aff8561cfd
janreiter793 <70901834+janreiter793@users.noreply.github.com>
2024-05-07 11:14:25 +0200

    Update obtainNu.R

--- obtainNu.R
+++ obtainNu.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(magrittr)
 
 # Parameters
-files <- c("C:/Users/janre/Documents/uni/8. Semester/Projekt/data/AMZN_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/BRK-B_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/GOOG_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/GOOGL_options.csv",
-           "C:/Users/janre/Documents/uni/8. Semester/Projekt/data/TSLA_options.csv")
+files <- c("~/data/AMZN_options.csv",
+           "~/data/BRK-B_options.csv",
+           "~/data/GOOG_options.csv",
+           "~/data/GOOGL_options.csv",
+           "~/data/TSLA_options.csv")
 
 # Takes an option symbol and returns the associated underlying asset symbol
 getUnderlyingSymbol <- function(symbol) {
